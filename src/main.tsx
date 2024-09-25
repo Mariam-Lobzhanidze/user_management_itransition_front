c851f64c701949d993bf060f1642365f85ded182
Mariam-Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2024-09-25 19:22:20 +0400

    on production

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+createRoot(document.getElementById("root")!).render(<App />);
